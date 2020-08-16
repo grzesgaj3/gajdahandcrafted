69a7af003cba29e6ef409b4bc3d2037705b7d2d6
grzesgaj3 <65665378+grzesgaj3@users.noreply.github.com>
2020-08-16 11:12:55 +0200

    New Mail service

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,26 +1,23 @@
 <?php
-// Check for empty fields
-if(empty($_POST['name'])      ||
-   empty($_POST['email'])     ||
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
 
-   empty($_POST['message'])   ||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-   echo "No arguments Provided!";
-   return false;
-   }
-   
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email_address = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
-   
-// Create the email and send the message
-$to = 'grzesgaj3@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "smutnygamer14@gmail.com"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
+$email_from = 'Test';
+
+$email_subject = "Stronka Wiad"
+
+$email_body = "User Name: $name.\n".
+                  "User Email: $visitor_email.\n".
+                     "User Message: $message.\n";
+
+$to = "grzesgaj3@gmail.com"
+
+$headers = "From : $email_from \r\n";
+
+$headers .="Reply-To: $visitor_email \r\n";
+
 mail($to,$email_subject,$email_body,$headers);
-return true;         
+
+header("Location: index.html");
 ?>
